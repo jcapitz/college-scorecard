ed20618412688370f92f315f1af0ced540b42399
ttran0 <ttran0@outlook.com>
2016-04-15 11:47:31 -0700

    admm cluster

--- cluster_admm.py
+++ cluster_admm.py
@@ -1,79 +1,100 @@
+import pandas as pd 
+import numpy as np
+
+
 def cluster(D):
-flag = 0
-m = D.shape[0]
-n = D.shape[1]
-r = np.zeros([m,n])
-
-x = np.random.rand([m,n])
-lam = np.random.rand([m,n])
-
-
-while(flag == 0):
-	r_new = min_r(x,ro,lam)
-	x_new = min_x(D,ro,lam,r_new,alpha)
-	lam = update_lam(lam,ro,r_new,x_new)
-	if (.....):
-		flag = 1
-	x = x_new
-
-return x
-
-
-
-def min_r(X,ro,lam):
+	flag = 0
 	m = D.shape[0]
 	n = D.shape[1]
-	for i in range(0,m):
-		for j = range(0,n):
-			x_hat = x[j,:] - x[i,:]
-			if x_hat + lam[i,j]/ro > 1./ro:
-				r[i,j] = (x_hat - lam[i,j]/ro)*( 1. - 1./(ro * (x_hat + lam[i,j]/ro ))
-			else:
-				r[i,j] = 0
-	return r
+	r = np.ones((m,m,n))
+	lamb = np.ones((m,m,n))
+	X = D
+	rho = 0.5
+	alpha = 0.5
+	iter = 0
 
-def rsolve(X,rho):
+	while True:
+		r_new = min_r(X,lamb,rho)
+		X_new = min_X(X,D,r_new,lamb,rho,alpha)
+		lamb_new = update_lamb(X_new,r_new,lamb,rho)
+
+		if (iter > 2):
+			# flag = 1
+			break
+
+		iter = iter + 1
+		print(iter)
+		X = X_new
+		lamb = lamb_new
+
+
+	return r_new,X_new
+
+
+def min_r(X,lamb,rho):
     
     m = X.shape[0] # row dimension from X
     n = X.shape[1] # colum dimemsion from X
     r = np.empty((m,m,n))
-    for i in xrange(m):
-        for j in xrange(m):
-            lamb = np.random.rand(n) # placeholder, need to create calling lambda solver
-            xhat = X[i] - X[j]
-            if np.linalg.norm(xhat + lamb/rho,2) > 1./rho:# 
-                r[i,j] = (xhat - lamb/rho) * (1. - 1./np.linalg.norm(xhat - 1./(rho * np.linalg.norm(xhat + lamb/rho))))
+    for i in range(m):
+        for j in range(m):
+            xhat = -(X[i] - X[j] + lamb[i,j]/rho) 
+            if np.linalg.norm(xhat,2) > 1./rho:
+                r[i,j] = xhat * (1. - 1./(rho*np.linalg.norm(xhat,2)))
             else:
                 r[i,j] = np.zeros(n)
                                               
             
     return r
 
-def min_x( D,ro,lam,r,alpha):
-	m = D.shape[0]
-	n = D.shape[1]
-	X = np.zeros([m,n])
-	s = np.zeros([m])
-	d_bar = np.sum(D,axis = 0)/n
-	for i in range (0,m):
-		for j in range(1,n):
-			if (j != i):
-				s[i] = np.sum(lam[i,j] + 2. * ro * r[i,j])
-				
-	gamma = np.sum(g)
-	x_bar = d_bar - gamma/alpha
-	for i in range(0,m):
-		X[i,:] = alpha/n * d[i,:] + 2 * ro * x_bar - 1/n * s[i]
+def min_X(X,D,r,lamb,rho,alpha):
+	m = X.shape[0]
+	n = X.shape[1]
+	X_new = np.zeros([m,n])
+	for i in range(m):
+		ind = list(range(m))
+		del ind[i]
+		gamma = 1./m*np.sum(lamb[i,ind] + 2. * rho * r[i,ind])
+		d_bar = np.sum(D)/m
+		x_bar = d_bar - gamma/alpha
+		X_new[i] = 1/(2.*rho + alpha/m) * ( (alpha/m) * D[i] + 2 * rho * x_bar - gamma)
+
+	return X_new
+
+
+def update_lamb(X,r,lamb,rho):
+	m = X.shape[0]
+	n = X.shape[1]
+	lamb_new = np.empty((m,m,n))
+	for i in range(m):
+		for j in range(m):
+			x_hat = X[i] - X[j]
+			lamb_new[i,j] = lamb[i,j] + rho * (r[i,j] + X[i] - X[j])
+	return lamb_new
+
+
+def get_clusters(X):
+	m = X.shape[0]
+	n = X.shape[1]
+	# indices = list(range(m))
+	labels = np.ones(m)
+	tol = 1e-2
+	X = np.concatenate((X,np.asarray(list(range(0,m))).reshape(m,1)),axis = 1) #ADD INDICES TO LAST COLUMN OF X
+	X = X[X[:,0].argsort()] #SORT X BY FIRST COLUMN
+
 	return X
 
-def update_lam(lam,ro,r,X):
-	m = D.shape[0]
-	n = D.shape[1]
-	new_lam = np.zeros([m,n])
-	for i in range(0,m):
-		for j in range(0,n):
-			new_lam[i,j] = lam[i,j] + ro * (r[i,j] + X[i,:] - X[j,:])
 
-	return new_lam
+df = pd.read_csv('PitchFxExample.csv')
+D = df.iloc[:,3:].values
+
+#GET X MATRIX 
+r_new, X = cluster(D)
+X_new = get_clusters(X)
+
+
+
+
+
 
 
